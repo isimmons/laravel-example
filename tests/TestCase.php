a2419f9a137b118bd13d8f841b31addd78241358
isimmons <isimmons@users.noreply.github.com>
2024-05-17 01:57:24 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Isimmons\Example\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Isimmons\Example\ExampleServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
